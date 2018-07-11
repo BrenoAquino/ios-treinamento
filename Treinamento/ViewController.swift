cd4fa71ee29d83a1273a62e93b0d9a06ae9e6fd6
Breno Aquino <breno@MacBook-Pro-de-Breno.local>
2018-07-11 12:07:32 -0300

    Initial Commit

--- Treinamento/ViewController.swift
+++ Treinamento/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Treinamento
 //
 //  Created by Breno Aquino on 11/07/18.
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginTitle: UILabel!
+    @IBOutlet weak var matriculaLegend: UILabel!
+    @IBOutlet weak var senhaLegend: UILabel!
+    @IBOutlet weak var matricula: UITextField!
+    @IBOutlet weak var senha: UITextField!
+    @IBOutlet weak var entrar: UIButton!
+    @IBOutlet weak var matriculaResult: UILabel!
+    @IBOutlet weak var senhaResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.loginTitle.text = "LOGIN"
+        self.matriculaLegend.text = "Entrar com Matricula"
+        self.senhaLegend.text = "Senha"
+        self.matriculaLegend.text = "Matricula Digitada"
+        self.senhaLegend.text = "Senha Digitada"
+        self.senha.textContentType = .password
+        
+        self.entrar.setTitle("Entrar", for: .normal)
+        self.entrar.setTitle("Entrando", for: .highlighted)
+        self.entrar.backgroundColor = .orange
+        self.entrar.layer.cornerRadius = self.entrar.frame.height / 2
     }
-
-
+    
+    @IBAction func logar(_ sender: Any) {
+        
+        self.matriculaResult.text = self.matricula.text
+        self.senhaResult.text = self.senha.text
+        
+        self.matricula.text = ""
+        self.senha.text = nil
+    }
 }
-
